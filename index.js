0667836cff6ac1684d22aaee8a4062bf1febbf39
Felyppe Tilly <felyppetilly@gmail.com>
2022-04-11 07:58:06 -0300

    implementacao da leitura de parametros pelo arquivo txt e estrutura de repeticao para editar todos os videos da lista

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-// const ffmpeg = require('fluent-ffmpeg');
 import editarVideo from './metodos/editarVideo';
 
-const video_path = './preparacao_ambiente.mp4';
-const vinheta_path = './vinheta.mp4';
-const nome_arquivo = 'final-teste-maquinanova.mp4'
+editarVideo();
 
-// function editarVideo(primeira_parte_video, segunda_parte_video, nome_final_arquivo) {
-//     ffmpeg({ source: primeira_parte_video })
-//         .input(segunda_parte_video)
-//         .on('end', () => console.log('Edição finalizada'))
-//         .on('error', (err) => console.log('Error:', err))
-//         .mergeToFile(nome_final_arquivo);
-// }
 
-editarVideo(video_path, vinheta_path, nome_arquivo);
+
+
+
+
+
 
--- metodos/editarVideo.js
+++ metodos/editarVideo.js
@@ -1,9 +1,37 @@
+import { readFile } from 'fs';
 const ffmpeg = require('fluent-ffmpeg');
+const content = './metodos/parametros.txt';
 
-export default function editarVideo(primeira_parte_video, segunda_parte_video, nome_final_arquivo) {
-    ffmpeg({ source: primeira_parte_video })
-        .input(segunda_parte_video)
-        .on('end', () => console.log('Edição finalizada'))
-        .on('error', (err) => console.log('Error:', err))
-        .mergeToFile(nome_final_arquivo);
+// export default function editarVideo() {
+
+//     readFile(content, 'utf-8', (err, data) => {
+//         var param = data.split(';');
+//         if (err) throw err;
+
+//         ffmpeg({ source: param[0] })
+//         .input(param[1])
+//         .on('end', () => console.log('Edição finalizada'))
+//         .on('error', (err) => console.log('Error:', err))
+//         .mergeToFile(param[2]);
+//     })
+// }
+
+
+export default function editarVideo() {
+
+    readFile(content, 'utf-8', (err, data) => {
+        var linhas = data.split(/\r?\n/);
+        if (err) throw err;
+
+        linhas.forEach((linha) => {
+            var param = linha.split(';');
+            console.log(param);
+
+            ffmpeg({ source: param[0] })
+                .input(param[1])
+                .on('end', () => console.log('Edição finalizada'))
+                .on('error', (err) => console.log('Error:', err))
+                .mergeToFile(param[2]);
+        })
+    })
 }
