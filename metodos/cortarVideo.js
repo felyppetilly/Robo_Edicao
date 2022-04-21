a257679bde5d3dcdb201db62c1369ca212ab8263
Felyppe Tilly <felyppetilly@gmail.com>
2022-04-21 09:46:55 -0300

    alteracoes

--- metodos/cortarVideo.js
+++ metodos/cortarVideo.js
@@ -1,7 +1,8 @@
 const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs');
 
 export default function cortarVideo() {
-    ffmpeg({ source: 'Aula_02_Git_Github.mp4' })
+    ffmpeg({ source: './arquivos/sem_edicao/Aula_02_Git_Github.mp4' })
         .setDuration('5:00.00')
         .save('Aula_02_Git_Github_Cortada.mp4')
 }
