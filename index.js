c4fdef5180edae3ec39a6f0b62e0d658279d27f0
Felyppe Witt Tilly <felyppetilly@brq.com>
2022-04-29 18:39:27 -0300

    alteracoes

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import lerParametros from './metodos/lerParametros';
+import { exec } from 'child_process';
 
-lerParametros('juntar');
+console.log("Iniciando edição")
+
+exec('ffmpeg -f concat -i cortes.txt aula02_editada.mp4', (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+    }
+    console.log("Video editado com sucesos as " + new Date());
+});
+
+// lerParametros('juntar');
 // lerParametros('cortar');
-
-
-
-
-
-
-
-
-
-
--- metodos/lerParametros.js
+++ metodos/lerParametros.js
@@ -3,16 +3,16 @@ import juntarVideo from './juntarVideo';
 const { readFile } = require('fs');
 var content;
 
-export default function lerParametros(acao){
+export default async function lerParametros(acao){
     acao == 'cortar' ? content = './arquivos/parametros/cortes.txt' : content = './arquivos/parametros/edicao.txt';
     
     readFile(content, 'utf-8', (err, data) => {
         var linhas = data.split(/\r?\n/);
         if (err) throw err;
 
-        linhas.forEach((linha) => {
+        for(const linha of linhas){
             var param = linha.split(';');
-            acao == 'cortar' ? cortarVideo(param) :  juntarVideo(param);
-        })
+            acao == 'cortar' ? cortarVideo(param) : juntarVideo(param);
+        }
     })
 }
