7b2168e82f466f2bf65b07b7740fb0d6ee1e5245
Felyppe Tilly <felyppetilly@gmail.com>
2022-04-29 20:19:51 -0300

    alteracoes

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 import lerParametros from './metodos/lerParametros';
-import { exec } from 'child_process';
+import juntarVideo from './metodos/juntarVideo';
 
-console.log("Iniciando edição")
+juntarVideo();
 
-exec('ffmpeg -f concat -i cortes.txt aula02_editada.mp4', (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-    }
-    console.log("Video editado com sucesos as " + new Date());
-});
+
 
 // lerParametros('juntar');
 // lerParametros('cortar');
--- metodos/juntarVideo.js
+++ metodos/juntarVideo.js
@@ -1,9 +1,15 @@
-const ffmpeg = require('fluent-ffmpeg');
+import { exec } from 'child_process';
 
-export default function juntarVideo(param) {
-    ffmpeg({ source: param[0] })
-        .input(param[1])
-        .on('end', () => console.log('Edição do video ' + param[2] + ' foi finalizada as ' + new Date()))
-        .on('error', (err) => console.log('Error:', err))
-        .mergeToFile('arquivos/editados/' + param[2]);
+export default function juntarVideo() {
+    console.log('Editando o video')
+    exec('ffmpeg -f concat -safe 0 -i ././arquivos/parametros/edicao.txt ./arquivos/editados/aula02_editada.mp4', (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+        }
+        console.log("Video editado com sucesos as " + new Date());
+    });
 }
